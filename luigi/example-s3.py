f765edd0da398cadafee99ae643248a5b6bfe04b
Ubuntu <ubuntu@ip-172-31-1-119.eu-west-1.compute.internal>
2016-11-14 14:08:31 +0000

    s3 fixed

--- luigi/example-s3.py
+++ luigi/example-s3.py
@@ -1,11 +1,12 @@
 import luigi
 from luigi.s3 import S3Target
 
-
 class MyS3File(luigi.ExternalTask):
 
     def output(self):
-        return luigi.S3Target('s3://s3-eu-west-1.amazonaws.com/jncc-data/example/hello.txt')
+        
+	return S3Target('s3://jncc-data/example/hello.txt')
+#	return S3Target('s3://s3-eu-west-1/jncc-data/example/hello.txt')
 # s3-eu-west-1.amazonaws.com/eodip/sentinel/S1A_IW_GRDH_1SDV_20151119T175834_20151119T175859_008677_00C566_A3A0.zip
 # https://s3-eu-west-1.amazonaws.com/jncc-data/example/hello.txt
 
@@ -17,7 +18,7 @@ class ProcessS3File(luigi.Task):
         return MyS3File()
 
     def output(self):
-        return luigi.S3Target(self.input().path + '.name_' + self.name)
+        return S3Target(self.input().path + '.name_' + self.name)
 
     def run(self):
         with self.input().open() as infile, self.output().open('w') as outfile:

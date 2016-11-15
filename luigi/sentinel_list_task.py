04a495dc192fbe77266b7c6fcd07976b64fa2332
Pete Montgomery <sumo.the.cat@gmail.com>
2016-11-15 10:11:39 +0000

    ideas

--- luigi/sentinel_list_task.py
+++ luigi/sentinel_list_task.py
@@ -15,6 +15,8 @@ class SentinelListTask(luigi.Task)
 # SentinelListTask/2016-11-13/available.json  (files to download)
 # query esa hub for all products ingested after latest ingestion date (in available.ljson)
 
+# [{"id":"dd","title":"S1_DDDD","attempts":"0"},{"id":"dd","title":"S1_DDDE","attempts":"0"}]
+# 
 # SentinelDownloadTask
 # ====================
 # write a properties.json with attempts=0 
@@ -22,8 +24,23 @@ class SentinelListTask(luigi.Task)
 # if succeeds, adds the properties and location to the properties.json 
 # SentinelDownloadTask/2016-11-13/S1A_IW_GRDH_1SDV_20151116T062140_20151116T062205_008626_00C401_1F80.json
 # all successfully downloaded files properties.json (plus s3 path location)
-
+sdt/[date]/S1_DDDD.json
+{"id":"dd","title":"S1_DDDD","attempts":"1","location":"s3://sentnel/raw/S1_DDDD.zip"}
+{"id":"dd","title":"S1_DDDE","attempts":"1","location":""}
 
 # SentinelListTask/2016-11-14/available.json  (files to download)
 
+# SentinelCatalogTask
+# ====================
+{"id":"dd","title":"S1_DDDD","attempts":"1","location":"s3://sentnel/raw/S1_DDDD.zip"}
+----> Catalog - writtten as record
+{"id":"dd","title":"S1_DDDE","attempts":"1","location":""}
+----> Ignored
 
+# 2nd run
+#
+# SentinelListTask
+# ================
+# 
+# 
+# 

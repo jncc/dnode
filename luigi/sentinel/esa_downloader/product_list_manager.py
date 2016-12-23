4fa4287700f4f7af8035867e8ae09b9b8755378d
felix mason <felix.mason@jncc.gov.uk>
2016-12-23 09:46:24 +0000

    Paging esa download

--- luigi/sentinel/esa_downloader/product_list_manager.py
+++ luigi/sentinel/esa_downloader/product_list_manager.py
@@ -4,6 +4,8 @@ import urllib
 import pycurl
 import xml.etree.ElementTree as eTree
 
+
+from functional import seq
 from datetime import datetime
 from StringIO import StringIO
 from config_manager import ConfigManager
@@ -26,7 +28,7 @@ class ProductListManager:
 
         return topDate
         
-    def __get_search_url(self, lastIngestionDate):
+    def __get_search_url(self, lastIngestionDate, page):
         ingestionDateString = lastIngestionDate.strftime('%Y-%m-%d') + 'T00:00:00.000Z'
         criteria = {'q': 'ingestiondate:[%s TO NOW] AND footprint:"Intersects(%s)"' % (ingestionDateString, ProductListManager.POLYGON)}
         url = ProductListManager.SEARCH_URL_BASE + '?' + urllib.urlencode(criteria)
@@ -52,11 +54,17 @@ class ProductListManager:
 
         return rawDataBuffer.getvalue()
 
-    def __add_products_to_list(self, rawProductsData, productList):
+    def __get_xml_element_tree(self, data):
+        root = None
         try:
-            root = eTree.fromstring(rawProductsData)
+            root = eTree.fromstring(data)
         except eTree.ParseError:
-            raise Exception("Parse Error: %s \n %s" % (eTree.ParseError.message, rawProductsData))
+            raise Exception("Parse Error: %s \n %s" % (eTree.ParseError.message, data))
+
+        return root
+        
+    def __add_products_to_list(self, rawProductsData, productList):
+        root = self.__get_xml_element_tree(rawProductsData)
 
         for entry in root.iter('{http://www.w3.org/2005/Atom}entry'):
             uniqueId = entry.find('{http://www.w3.org/2005/Atom}id').text
@@ -106,12 +114,6 @@ class ProductListManager:
                 }
         
             productList["products"].append(product)
-
-
-    def __collect_product_ids(self, productList):
-        productIds = []
-        for product in productList["products"]:
-            productIds.append(product["uniqueId"])
     
     def __check_downloads_in_cat(self, productIds):
         dowloadedProductIds = ["31ed1d48-3e05-4156-9ec2-7bf17e98802e"]
@@ -120,6 +122,19 @@ class ProductListManager:
         # result would be ["31ed1d48-3e05-4156-9ec2-7bf17e98802e","05dabcc8-bb50-4645-b215-43b46a6e4bf2e"]
         return dowloadedProductIds
 
+    def __getPages(self, rawProductsData):
+        root = self.__get_xml_element_tree(rawProductsData)
+
+        pages = 1
+
+        totalResults = int(root.find('{http://a9.com/-/spec/opensearch/1.1/}totalResults').text)
+
+        if totalResults > 100:
+            pages = int(math.ceil(totalResults / 100))
+        
+        return pages
+
+    
 
     def create_list(self,runDate, lastListFile, outputListFile):
         productList = json.load(lastListFile)
@@ -129,16 +144,29 @@ class ProductListManager:
         if (runDate - lastIngestionDate).days > 3:
             raise Exception("Last ingestion date older then 3 days")
 
-        searchUrl = self.__get_search_url(lastIngestionDate)
+        page = 1
+        pages = 1
 
+        searchUrl = self.__get_search_url(lastIngestionDate, page)
         rawProductsData = self.__get_xml_data(searchUrl)
 
-        self.__add_products_to_list(rawProductsData, productList)
+        pages = self.__getPages(rawProductsData)
 
-        productIds = self.__collect_product_ids(productList)
+        while page <= pages:
+            self.__add_products_to_list(rawProductsData, productList)
+            page = page + 1
 
+            searchUrl = self.__get_search_url(lastIngestionDate, page)
+            rawProductsData = self.__get_xml_data(searchUrl)
+            if rawProductsData = None:
+                break
+
+        productIds = (seq(productList["products"])
+                    .select(lambda x: x["uniqueId"])
+                    .distinct()).to_list()
+        
         downloadedProductIds = self.__check_downloads_in_cat(productIds)
-
+        
         #remove downloaded products from list
         productList["products"] = [product for product in productList["products"] if product["uniqueId"] not in downloadedProductIds]
 

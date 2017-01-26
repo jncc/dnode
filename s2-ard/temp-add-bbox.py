1f82a5daf2203994df7106e3e12da367d7846d29
Pete Montgomery <sumo.the.cat@gmail.com>
2017-01-26 16:30:11 +0000

    add bbox to existing s2 ard products

--- s2-ard/temp-add-bbox.py
+++ s2-ard/temp-add-bbox.py
@@ -1,8 +1,11 @@
 
+# pip install pyfunctional
+# pip install shapely
 
 import json
 from pprint import pprint
 from functional import seq
+from shapely.geometry import shape
 
 with open('products-v1.json') as datafile:
     data = json.load(datafile)
@@ -10,8 +13,19 @@ with open('products-v1.json') as datafile:
 pprint(data)
 print(len(data))
 
+
+# (minx, miny, maxx, maxy)
+def getBBox(geom):
+    return shape(geom).bounds
+    
 (seq(data)
-    .map(lambda p: { blah: p["id"] })
+    .map(lambda p: { "id"   : p["id"],
+             "title" : p["title"],
+             "footprint": p["footprint"],
+             "bbox": getBBox(p["footprint"]["geometry"]),
+             "properties": p["properties"],
+             "representations": p["representations"]
+    })
 .to_json('products-v1-withbbox.json'))
 
 

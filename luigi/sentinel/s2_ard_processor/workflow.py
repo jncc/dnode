d410c3e46f70fddabd5b4c4beee23b469d3f513c
felix mason <felix.mason@jncc.gov.uk>
2017-01-26 11:42:18 +0000

    working test task

--- luigi/sentinel/s2_ard_processor/workflow.py
+++ luigi/sentinel/s2_ard_processor/workflow.py
@@ -7,7 +7,7 @@ import json
 from luigi.util import requires
 
 #FILE_ROOT = 's3://jncc-data/workflows/sentinel-download/'
-FILE_ROOT = '/home/felix/temp/esadownloader'
+FILE_ROOT = '/home/felix/temp/s2ard'
 #Create job spec
 class CreateJobSpec(luigi.Task):
     text = luigi.Parameter()
@@ -18,8 +18,10 @@ class CreateJobSpec(luigi.Task):
            jobspec.write(json.dumps(job))
 
     def output(self):
+        d = datetime.datetime.now()
         filePath = os.path.join(os.path.join(FILE_ROOT, d.strftime("%Y-%m-%d")), 'job.json')
-        
+        print 
+
         return luigi.LocalTarget(filePath)
 
 #Process using docker isinstance
@@ -27,10 +29,12 @@ class CreateJobSpec(luigi.Task):
 class CreateArdProduct(luigi.Task):
     
     def run(self):
-        with self.output().open('w') as output:
-            output.write('test')
+        with self.input().open() as jobspec, self.output().open('w') as output:
+            job = json.load(jobspec)
+            output.write(job["text"])
         
     def output(self):
+        d = datetime.datetime.now()
         filePath = os.path.join(os.path.join(FILE_ROOT, d.strftime("%Y-%m-%d")), 'output.txt')
 
         return luigi.LocalTarget(filePath)

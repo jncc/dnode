fdb21b5835558fdd0c6baf2f55db71bca23c755e
Matt Debont <matt.debont@jncc.gov.uk>
2017-02-15 15:02:13 +0000

    Fixed bugs with artifical bucket key limits, double remote paths and missing slashes

--- luigi/sentinel/s1_ard_downloader/product_inventory_checker.py
+++ luigi/sentinel/s1_ard_downloader/product_inventory_checker.py
@@ -29,17 +29,17 @@ class ProductInventoryChecker:
         amazon_key_secret = self.s3_conf['secret_access_key']
 
         conn = boto.s3.connect_to_region(self.s3_conf['region'], aws_access_key_id=amazon_key_Id, aws_secret_access_key=amazon_key_secret, is_secure=True)
-        bucket = conn.get_bucket(self.s3_conf['bucket'])  
+        bucket = conn.get_bucket(self.s3_conf['bucket'])
 
         if remote_extra_path is not None:
-            keys = bucket.get_all_keys(prefix=os.path.join(remote_path, remote_extra_path))
+            prefix = os.path.join(remote_path, remote_extra_path)
         else: 
-            keys = bucket.get_all_keys(prefix=remote_path)
+            prefix = remote_path
 
         exp = re.compile('(%s\/20[0-9]{2}\/[0-9]{2}\/(.*\.SAFE\.data))(.*)' % re.escape(remote_path))
         groups = {}
 
-        for key in keys:
+        for key in bucket.list(prefix=prefix):
             res = exp.findall(key.key)
             if len(res) > 0:
                 name = res[0][1]
@@ -47,7 +47,7 @@ class ProductInventoryChecker:
                     groups[name]['keys'].append(key)
                 else:
                     groups[name] = {
-                        'path': res[0][0]
+                        'path': res[0][0],
                         'keys': [key]
                     }
         
@@ -88,11 +88,11 @@ class ProductInventoryChecker:
 
                         # Write out temporary file for now with corrected header
                         with open(os.path.join(self.temp, 'footprint_osni.geojson'), 'w') as osni_footprint_file:
-                            osni_footprint_file.write(footprint_osni)
+                            json.dump(footprint_osni, osni_footprint_file)
                         # Upload to correct location
                         dest_remote_path = os.path.join(path, os.path.join('OSNI1952', os.path.join('Footprint', '%s_OSNI1952_footprint.geojson' % (key))))
-                        s3Helper.copy_file_to_s3(self.logger, amazon_key_Id, amazon_key_secret, self.s3_conf['region'], bucket, remote_path, os.path.join(self.temp, 'footprint_osni.geojson'), dest_remote_path, True, None)
-                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], os.path.join(remote_path, dest_remote_path), s3Helper.get_file_type('.geojson')))
+                        s3Helper.copy_file_to_s3(self.logger, amazon_key_Id, amazon_key_secret, self.s3_conf['region'], bucket, '', os.path.join(self.temp, 'footprint_osni.geojson'), dest_remote_path, True, None)
+                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], os.path.join('/%s' % remote_path, dest_remote_path), s3Helper.get_file_type('.geojson')))
                     elif fkey.key.endswith('_metadata.xml'):
                         # Deal with metadata file
                         with open(os.path.join(self.temp, 'metadata_osni.xml'), 'wb') as metadata:
@@ -100,15 +100,15 @@ class ProductInventoryChecker:
                         with open(os.path.join(self.temp, 'metadata_osni.xml'), 'r') as metadata:
                             metadata_osni = metadataHelper.xml_to_json(metadata)
                             found_data['osni']['metadata'] = True
-                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
                     elif fkey.key.endswith('.tif'):
                         # Found data file
                         found_data['osni']['data'] = True
-                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
                     elif fkey.key.endswith('_quicklook.jpg'):
                         # Found quicklook
                         found_data['osni']['quicklook'] = True
-                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        osni_representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
                 else:
                     # Process OSGB data
                     if fkey.key.endswith('.json'):
@@ -119,11 +119,11 @@ class ProductInventoryChecker:
 
                         # Write out temporary file for now with corrected header
                         with open(os.path.join(self.temp, 'footprint_osgb.geojson'), 'w') as osgb_footprint_file:
-                            osgb_footprint_file.write(footprint_osgb)
+                            json.dump(footprint_osgb, osgb_footprint_file)
                         # Upload to correct location
                         dest_remote_path = os.path.join(path, os.path.join('Footprint', '%s_footprint.geojson' % (key)))
-                        s3Helper.copy_file_to_s3(self.logger, amazon_key_Id, amazon_key_secret, self.s3_conf['region'], bucket, remote_path, os.path.join(self.temp, 'footprint_osgb.geojson'), dest_remote_path, True, None)      
-                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], os.path.join(remote_path, dest_remote_path), s3Helper.get_file_type('.geojson')))
+                        s3Helper.copy_file_to_s3(self.logger, amazon_key_Id, amazon_key_secret, self.s3_conf['region'], bucket, '', os.path.join(self.temp, 'footprint_osgb.geojson'), dest_remote_path, True, None)      
+                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], os.path.join('/%s' % remote_path, dest_remote_path), s3Helper.get_file_type('.geojson')))
                     elif fkey.key.endswith('_metadata.xml'):
                         # Deal with metadata file
                         with open(os.path.join(self.temp, 'metadata_osgb.xml'), 'wb') as metadata:
@@ -131,24 +131,24 @@ class ProductInventoryChecker:
                         with open(os.path.join(self.temp, 'metadata_osgb.xml'), 'r') as metadata:                            
                             metadata_osgb = metadataHelper.xml_to_json(metadata)                        
                             found_data['osgb']['metadata'] = True
-                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
                     elif fkey.key.endswith('.tif'):
                         # Found data file
                         found_data['osgb']['data'] = True
-                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
                     elif fkey.key.endswith('_quicklook.jpg'):
                         # Found quicklook
                         found_data['osgb']['quicklook'] = True
-                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
+                        representations['s3'].append(s3Helper.get_representation(self.s3_conf['bucket'], self.s3_conf['region'], '/%s' % fkey.key, s3Helper.get_file_type(os.path.splitext(fkey.key)[1])))
 
             # Deal with OSGB product
             if found_data['osgb']['data'] and found_data['osgb']['metadata'] and found_data['osgb']['quicklook'] and found_data['osgb']['footprint']:
-                databaseHelper.write_progress_to_database(self.db_conn, self.database_conf['collection_version_uuid'], {'s3imported':True, 'filename':key}, metadata_osgb, representations, footprint_osgb)
+                databaseHelper.write_progress_to_database(self.db_conn, self.database_conf['collection_version_uuid'], {'s3imported':True, 'filename':key}, metadata_osgb, representations, footprint_osgb, None)
                 # Do Cleanup
             
             # Deal with OSNI product
             if found_data['osni']['data'] and found_data['osni']['metadata'] and found_data['osni']['quicklook'] and found_data['osni']['footprint']:
-               databaseHelper.write_progress_to_database(self.db_conn, self.database_conf['collection_version_uuid'], {'s3imported':True, 'filename':key}, metadata_osni, osni_representations, footprint_osni, additional={'relatedTo': metadata_osgb['ID']})
+               databaseHelper.write_progress_to_database(self.db_conn, self.database_conf['collection_version_uuid'], {'s3imported':True, 'filename':key}, metadata_osni, osni_representations, footprint_osni, {'relatedTo': metadata_osgb['ID']})
                # Do Cleanup
 
     def cleanupPath(self, path, name, bucket, source_bucket_name, osni):

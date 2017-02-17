79f8c25b6339d305991f2e7df151fe1778973142
Paul Gilbertson <paul.gilbertson@gmail.com>
2017-02-17 16:12:23 +0000

    S3 support

--- luigi/meo-ap/workflow.py
+++ luigi/meo-ap/workflow.py
@@ -8,8 +8,11 @@ from ftp_client import FTPClient
 from config_manager import ConfigManager
 from catalog_manager import CatalogManager
 from luigi.util import requires
+from luigi.s3 import S3Target
 
-#FILE_ROOT = 's3://jncc-data/workflows/s2ard/'
+from helpers import s3 as s3Helper
+
+S3_FILE_ROOT = 's3://jncc-data/meo-ap/chlor_a/'
 FILE_ROOT = '/tmp/meo-ap'
 #DOCKER_IMAGE = '914910572686.dkr.ecr.eu-west-1.amazonaws.com/process-test'
 PRODUCTLIST = ['daily', '5day', 'monthly']
@@ -30,9 +33,9 @@ class CreateWorkOrder(luigi.Task):
             json.dump(plist, wddump, indent=4, sort_keys=True, separators=(',', ':'))    
     
     def output(self):
-       filePath = os.path.join(os.path.join(FILE_ROOT, self.runDate.strftime("%Y-%m-%d")), 'list.json')  
+       filePath = os.path.join(S3_FILE_ROOT, 'todo.json')  
 
-       return luigi.LocalTarget(filePath)
+       return S3Target(filePath)
 
 class TransformSrcFileToTiff(luigi.Task):
     runDate = luigi.DateParameter(default=datetime.datetime.now())
@@ -42,24 +45,27 @@ class TransformSrcFileToTiff(luigi.Task):
 
     ftp = FTPClient()
     catalog = CatalogManager()
+    config = ConfigManager('cfg.ini')
 
     def run(self):
         ncFile = os.path.join(os.path.join(FILE_ROOT, self.runDate.strftime("%Y-%m-%d")), self.product + '-' + self.fileDate + '.nc')
-        tiffFile = os.path.join(os.path.join(FILE_ROOT, self.runDate.strftime("%Y-%m-%d")), 'UK-' + self.product + '-' + self.fileDate + '.tiff')
+        tiffFile = os.path.join(os.path.join(FILE_ROOT, self.runDate.strftime("%Y-%m-%d")), 'UK-' + self.product + '-' + self.fileDate + '.tif')
+        s3DstFile = os.path.join(os.path.join(os.path.join(os.path.join('meo-ap/chlor_a', self.product), self.fileDate[:4]), self.fileDate[5:6]), 'UK-' + self.product + '-' + self.fileDate + '.tif')
+        httpLoc = 'http://jnnc-data.s3-eu-west-1.amazonaws.com/' + s3DstFile
 
         print('Retrieving ' + self.srcFile)
         self.ftp.getFile(self.product, self.srcFile, ncFile)
 
         os.system('gdal_translate NETCDF:' + ncFile + ':chlor_a -projwin -24 63 6 48 ' + tiffFile)
 
-        self.catalog.addEntry(self.product, 'Chlorophyll-A Density for UK Waters - ' + self.product + ' - ' + self.fileDate, self.srcFile, tiffFile, datetime.datetime.now().strftime("%Y-%m-%d"))
-        with self.output().open('w') as outp:
-            outp.write('Test\n')
+        s3Helper.copy_file_to_s3(self.config.getAmazonKeyId(), self.config.getAmazonKeySecret(), self.config.getAmazonRegion(), self.config.getAmazonBucketName(), tiffFile, s3DstFile, True, None)
+
+        self.catalog.addEntry(self.product, 'Chlorophyll-A Density for UK Waters - ' + self.product + ' - ' + self.fileDate, self.srcFile, httpLoc, datetime.datetime.now().strftime("%Y-%m-%d"))
 
         return
 
     def output(self):
-        filePath = os.path.join(os.path.join(FILE_ROOT, self.runDate.strftime("%Y-%m-%d")), self.product + '-' + self.fileDate + '.tmp')  
+        filePath = os.path.join(os.path.join(os.path.join(os.path.join(S3_FILE_ROOT, self.product), self.fileDate[:4]), self.fileDate[5:6]), 'UK-' + self.product + '-' + self.fileDate + '.tif')
 
         return luigi.LocalTarget(filePath) 
 

ef2181ed6f10cdb54831651239dc51425ca9f8f2
felix mason <felix.mason@jncc.gov.uk>
2017-02-21 13:18:34 +0000

    s2 ard processor build

--- luigi/sentinel/s2_ard_processor/docker_image/app/exec.sh
+++ luigi/sentinel/s2_ard_processor/docker_image/app/exec.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# python process.py
-
-# Stress test
-# stress-ng --cpu 2 --io 2 --vm 1 --vm-bytes 1G --timeout 15m --metrics-brief > /mnt/state/output.txt
-
-chown -R $USERID:$GROUPID /mnt/state
+source activate osgeoenv
+export CPL_ZIP_ENCODING=UTF-8
+sh ./S2PreProcessing_V3/Pre_Processing.sh

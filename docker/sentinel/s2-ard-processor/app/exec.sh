8c7407bc04433816ff0875746b95e2d16c4f5d72
Felix Mason <felix.mason@jncc.gov.uk>
2017-04-13 09:36:24 +0100

    Re-enable upload and download scripts on exec

--- docker/sentinel/s2-ard-processor/app/exec.sh
+++ docker/sentinel/s2-ard-processor/app/exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source activate osgeoenv
 export CPL_ZIP_ENCODING=UTF-8
-#/app/s3download
+python /app/s3download
 cd S2PreProcessing_V3
 sh ./Pre_Processing.sh
-#/app/s3upload
+phython /app/s3upload

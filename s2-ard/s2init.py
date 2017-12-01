bc18c363cdb1f39911c232f37ce0a6e008cc0aca
Matt Debont <matt.debont@jncc.gov.uk>
2017-12-01 14:45:49 +0000

    Almost working generation of HTML

--- s2-ard/s2init.py
+++ s2-ard/s2init.py
@@ -7,6 +7,8 @@ import re
 import subprocess
 import time
 
+logger = logging.getLogger('s2init')
+
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
@@ -36,7 +38,7 @@ def add_item(output, matched, satellite, year, month, day, lat, lon, grid, orbit
             if new_projection is not None:
                 output[year][month][day][grid]['new_projection']: new_projection
 
-        if file_type is 'vmsk_sharp_rad_srefdem_stdsref':
+        if file_type == 'vmsk_sharp_rad_srefdem_stdsref':
             output[year][month][day][grid]['product'] = {
                 'data': matched,
                 'size': sizeof_fmt(remote.size)
@@ -50,20 +52,21 @@ def add_item(output, matched, satellite, year, month, day, lat, lon, grid, orbit
         if not grid in output:
             output[grid] = {}
 
-        output[grid]['%s%s%s' % (year, month, day)] = {
-            'name': 'S2%s_%s%s%s_lat%slon%s_T%s_ORB%s_%s%s' % (satellite, year, month, day, lat, lon, grid, orbit, original_projection, ('_%s' % (new_projection) if new_projection is not None else '')),
-            'satellite': 'sentinel-2%s' % (satellite.lower()),
-            'lat': lat,
-            'lon': lon,
-            'orbit': orbit,
-            'original_projection': original_projection,
-            'new_projection': original_projection       # Not a typo, this happens with Rockall
-        }
+        if not '%s%s%s' % (year, month, day) in output[grid]:
+            output[grid]['%s%s%s' % (year, month, day)] = {
+                'name': 'S2%s_%s%s%s_lat%slon%s_T%s_ORB%s_%s%s' % (satellite, year, month, day, lat, lon, grid, orbit, original_projection, ('_%s' % (new_projection) if new_projection is not None else '')),
+                'satellite': 'sentinel-2%s' % (satellite.lower()),
+                'lat': lat,
+                'lon': lon,
+                'orbit': orbit,
+                'original_projection': original_projection,
+                'new_projection': original_projection       # Not a typo, this happens with Rockall
+            }
 
         if new_projection is not None:
             output[grid]['%s%s%s' % (year, month, day)]['new_projection']: new_projection
 
-        if file_type is 'vmsk_sharp_rad_srefdem_stdsref':
+        if file_type == 'vmsk_sharp_rad_srefdem_stdsref':
             output[grid]['%s%s%s' % (year, month, day)]['product'] = {
                 'data': matched,
                 'size': sizeof_fmt(remote.size)
@@ -123,12 +126,11 @@ if __name__ == "__main__":
     output_date = {}
     output_grid = {}
 
-    regex = re.compile('Sentinel2([AB])\_((20[0-9]{2})([0-9]{2})([0-9]{2}))\/SEN2\_[0-9]{8}\_lat([0-9]{2,4})lon([0-9]{2,4})\_T([0-9]{2}[A-Z]{3})\_ORB([0-9]{3})\_(utm[0-9]{2}n)(\_osgb)?\_(clouds|sat|toposhad|valid|vmsk_sharp_rad_srefdem_stdsref|meta|thumbnail)(?!\.xml)')
+    regex = re.compile('Sentinel2([AB])\_((20[0-9]{2})([0-9]{2})([0-9]{2}))\/SEN2\_[0-9]{8}\_lat([0-9]{2,4})lon([0-9]{2,4})\_T([0-9]{2}[A-Z]{3})\_ORB([0-9]{3})\_(utm[0-9]{2}n)(\_osgb)?\_(clouds|sat|toposhad|valid|vmsk_sharp_rad_srefdem_stdsref|meta|thumbnail)(?!\.tif\.aux\.xml)')
 
     logger.info('Scanning bucket (%s) path \'%s\' for files' % (args.bucket, args.input))
     for remote in bucket.objects.filter(Prefix=args.input):
         match = regex.search(remote.key)
-        logger.info(remote.key)
         if match:
             satellite = match.group(1)
             full_date = match.group(2)
@@ -145,9 +147,7 @@ if __name__ == "__main__":
 
             # do something
             output_date = add_item(output_date, remote.key, satellite, year, month, day, lat, lon, grid, orbit, original_projection, new_projection, file_type, remote, year_slicing=True)
-            logger.info(output_date)
             output_grid = add_item(output_grid, remote.key, satellite, year, month, day, lat, lon, grid, orbit, original_projection, new_projection, file_type, remote, year_slicing=False)
-            logger.info(output_grid)
 
     date_temp_path = os.path.join(args.tempdir, 'date')
     grid_temp_path = os.path.join(args.tempdir, 'grid')
@@ -176,29 +176,28 @@ if __name__ == "__main__":
         
         for year in output_date:
             index.write('<tr>\n')
-            index.write('<td width=25%>%s</td>\n' % (year))
+            index.write('<td width=25%%>%s</td>\n' % (year))
             index.write('<td>\n')
             for month in output_date[year]:
-                index.write('<a href="%s/%s">%s</a><br/>\n' % (year, month, calendar.month_name[int(month)]))
+                index.write('<a href="%s/%s.html">%s</a><br/>\n' % (year, month, calendar.month_name[int(month)]))
                 if not os.path.exists(os.path.join(date_temp_path, year)):
                     os.makedirs(os.path.join(date_temp_path, year))
                 with open(os.path.join(os.path.join(date_temp_path, year), '%s.html' % month), 'w') as month_index:
                     month_index.write('<table>\n')
-                    month_index.write('<tr>\n')
                     for day in output_date[year][month]:
-                        month_index.write('<td width=10%>%s</td>\n' % (day))
                         for grid in output_date[year][month][day]:
+                            month_index.write('<tr>\n')
                             # month_index.write('<td><img src="%s" height=100px width=100px></td>\n' % (output_date[year][month][day][grid]['thumbnail']['data']))
                             month_index.write('<td>\n')
-                            month_index.write('<h3>%s</h3><br/>\n' % (output_date[year][month][day][grid]['name']))
+                            month_index.write('<h3>%s</h3>\n' % (output_date[year][month][day][grid]['name']))
                             month_index.write('<a href="%s">Data [GeoTIFF] (%s)</a><br/>\n' % (output_date[year][month][day][grid]['product']['data'], output_date[year][month][day][grid]['product']['size']))
                             month_index.write('<a href="%s">Cloudmask [GeoTIFF] (%s)</a><br/>\n' % (output_date[year][month][day][grid]['clouds']['data'], output_date[year][month][day][grid]['clouds']['size']))
                             month_index.write('<a href="%s">Saturated pixel mask [GeoTIFF] (%s)</a><br/>\n' % (output_date[year][month][day][grid]['sat']['data'], output_date[year][month][day][grid]['sat']['size']))
                             month_index.write('<a href="%s">Valid pixel mask [GeoTIFF] (%s)</a><br/>\n' % (output_date[year][month][day][grid]['valid']['data'], output_date[year][month][day][grid]['valid']['size']))
                             month_index.write('<a href="%s">Topographic shadow mask [GeoTIFF] (%s)</a><br/>\n' % (output_date[year][month][day][grid]['toposhad']['data'], output_date[year][month][day][grid]['toposhad']['size']))
-                            month_index.write('<a href="%s">Metadata [JSON] (%s)</a>\n' % (output_date[year][month][day][grid]['metadata']['data'], output_date[year][month][day][grid]['metadata']['size']))
+                            month_index.write('<a href="%s">Metadata [JSON] (%s)</a>\n' % (output_date[year][month][day][grid]['meta']['data'], output_date[year][month][day][grid]['meta']['size']))
                             month_index.write('</td>\n')
-                    month_index.write('</tr>\n')
+                            month_index.write('</tr>\n')
                     month_index.write('</table>\n')
             index.write('</td>\n')
             index.write('</tr>\n')
@@ -223,12 +222,12 @@ if __name__ == "__main__":
             index.write('<a href="%s/index.html">%s</a><br/>\n' % (grid, grid))
             if not os.path.exists(os.path.join(grid_temp_path, grid)):
                 os.makedirs(os.path.join(grid_temp_path, grid))
-            with open(os.path.join(os.path.join(date_temp_path, year), 'index.html'), 'w') as grid_index:
+            with open(os.path.join(os.path.join(grid_temp_path, grid), 'index.html'), 'w') as grid_index:
                 grid_index.write('<h1>%s</h1>' % grid)
                 grid_index.write('<table>\n')
                 grid_index.write('<tr>\n')
                 for date in output_grid[grid]:
-                    grid_index.write('<td width=10%>%s</td>\n' % (day))
+                    grid_index.write('<td width=15%%>%s-%s-%s</td>\n' % (day, month, year))
                     # grid_index.write('<td><img src="%s" height=100px width=100px></td>\n' % (output_grid[grid][date]['thumbnail']['data']))
                     grid_index.write('<td>\n')
                     grid_index.write('<h3>%s</h3><br/>\n' % (output_grid[grid][date]['name']))
@@ -237,7 +236,7 @@ if __name__ == "__main__":
                     grid_index.write('<a href="%s">Saturated pixel mask [GeoTIFF] (%s)</a><br/>\n' % (output_grid[grid][date]['sat']['data'], output_grid[grid][date]['sat']['size']))
                     grid_index.write('<a href="%s">Valid pixel mask [GeoTIFF] (%s)</a><br/>\n' % (output_grid[grid][date]['valid']['data'], output_grid[grid][date]['valid']['size']))
                     grid_index.write('<a href="%s">Topographic shadow mask [GeoTIFF] (%s)</a><br/>\n' % (output_grid[grid][date]['toposhad']['data'], output_grid[grid][date]['toposhad']['size']))
-                    grid_index.write('<a href="%s">Metadata [JSON] (%s)</a>\n' % (output_grid[grid][date]['metadata']['data'], output_grid[grid][date]['metadata']['size']))
+                    grid_index.write('<a href="%s">Metadata [JSON] (%s)</a>\n' % (output_grid[grid][date]['meta']['data'], output_grid[grid][date]['meta']['size']))
                     grid_index.write('</td>\n')
                 grid_index.write('</tr>\n')
                 grid_index.write('</table>\n')

7068c23eea7092b8b10d62029af300f532cf4acd
Pete Montgomery <sumo.the.cat@gmail.com>
2017-12-13 11:48:16 +0000

    wip on thumbnails

--- s2-ingestion/thumbnails.py
+++ s2-ingestion/thumbnails.py
@@ -30,25 +30,23 @@ def main():
         product_file = next((f for f in files if f['type']=='product'), None)
         print(product_file)
         if product_file is not None:
+            # s3_bucket = 'eocoe-sentinel-2'
+            # s3_key = product_file['data']
+            # product_file_name = os.path.basename(s3_key)
+            # print(product_file_name)
+            # full_out_dir = os.path.join('.', outdir, 'thumbnails')
+            # local_product_path = os.path.join(full_out_dir, product_file_name)
+            # if not os.path.exists(full_out_dir):
+            #     os.makedirs(full_out_dir)
+            # s3_client.download_file(s3_bucket, s3_key, local_product_path)
+            # local_thumbnail_path = os.path.join(full_out_dir, thumbnail_file_name)
             create_thumbnail_x(s3_client, product_file, args.outdir)
 
 
-def create_thumbnail_x(s3_client, product_file, outdir):
-    s3_bucket = 'eocoe-sentinel-2'
-    s3_key = product_file['data']
-    product_file_name = os.path.basename(s3_key)
-    print(product_file_name)
-    full_out_dir = os.path.join('.', outdir, 'thumbnails')
-    local_product_path = os.path.join(full_out_dir, product_file_name)
-    if not os.path.exists(full_out_dir):
-        os.makedirs(full_out_dir)
-    s3_client.download_file(s3_bucket, s3_key, local_product_path)
-
-    thumbnail_file_name = product_file_name.replace('_vmsk_sharp_rad_srefdem_stdsref.tif', '_thumbnail.jpg')
-    local_thumbnail_path = os.path.join(full_out_dir, thumbnail_file_name)
-    print(local_product_path)
-    print(local_thumbnail_path)
-    shell_command = 'gdal_translate -b 3 -b 2 -b 1 -ot Byte -of JPEG -outsize 5%% 5%% %s %s' % (local_product_path, local_thumbnail_path)
+# def create_thumbnail_x(s3_client, product_file, outdir):
+def create_single_thumbnail(input_path):
+    output_path = input_path.replace('_vmsk_sharp_rad_srefdem_stdsref.tif', '_thumbnail.jpg')
+    shell_command = 'gdal_translate -b 3 -b 2 -b 1 -ot Byte -of JPEG -outsize 5%% 5%% %s %s' % (input_path, output_path)
     p = subprocess.Popen(shell_command, shell=True)
     (output, err) = p.communicate()
     if output is not None:
---
+++ s2-ingestion/thumbs/thumbnail.py
@@ -0,0 +1,25 @@
+
+import argparse
+import os
+import subprocess
+
+def parse_command_line_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('-i', '--input', type=str, required=True, help='Input file path')
+    return p.parse_args()
+
+def main():
+    args = parse_command_line_args()
+    create_single_thumbnail(args.input)
+
+def create_single_thumbnail(input_path):
+    output_path = input_path.replace('_vmsk_sharp_rad_srefdem_stdsref.tif', '_thumbnail.jpg')
+    shell_command = 'gdal_translate -b 3 -b 2 -b 1 -ot Byte -of JPEG -outsize 5%% 5%% %s %s' % (input_path, output_path)
+    p = subprocess.Popen(shell_command, shell=True)
+    (output, err) = p.communicate()
+    if output is not None:
+        self.log.debug(output)
+    if err is not None:
+        raise RuntimeError(err)
+
+main()

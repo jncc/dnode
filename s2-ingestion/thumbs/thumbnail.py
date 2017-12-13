2859de6b8c82bae5f83e73d53d14dddcd52d974e
Pete Montgomery <sumo.the.cat@gmail.com>
2017-12-13 20:48:59 +0000

    fix assertion

--- s2-ingestion/thumbs/thumbnail.py
+++ s2-ingestion/thumbs/thumbnail.py
@@ -14,7 +14,7 @@ def main():
 
 def create_single_thumbnail(input_path):
     product_ending = '_vmsk_sharp_rad_srefdem_stdsref.tif'
-    assert product_ending not in input_path
+    assert product_ending in input_path
     output_path = input_path.replace(product_ending, '_thumbnail.jpg')
     shell_command = 'gdal_translate -b 3 -b 2 -b 1 -ot Byte -of JPEG -outsize 5%% 5%% %s %s' % (input_path, output_path)
     p = subprocess.Popen(shell_command, shell=True)

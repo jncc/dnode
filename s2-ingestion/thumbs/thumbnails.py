3238584cc2d461328983265d28f9306dd1d49457
Pete Montgomery <sumo.the.cat@gmail.com>
2017-12-14 09:54:13 +0000

    add public-read ACL

--- s2-ingestion/thumbs/thumbnails.py
+++ s2-ingestion/thumbs/thumbnails.py
@@ -42,7 +42,7 @@ def main():
             # now there should be a thumbnail next to the product file!
             thumbnail_file_name = os.path.basename(thumbnail_path)
             thumbnail_s3_key = 'thumbnails/' + thumbnail_file_name
-            s3.Bucket(s3_bucket).upload_file(thumbnail_path, thumbnail_s3_key)
+            s3.Bucket(s3_bucket).upload_file(thumbnail_path, thumbnail_s3_key, ExtraArgs={'ACL':'public-read'})
             
 
 main()

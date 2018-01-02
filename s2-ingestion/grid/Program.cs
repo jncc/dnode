fa56fab9af6a33c2df80b563b2818e3a7df020f9
Pete Montgomery <sumo.the.cat@gmail.com>
2018-01-02 11:50:23 +0000

    query the grid geojson

---
+++ s2-ingestion/grid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            string json = File.ReadAllText(@"./s2ukwidegrid.json");
+            var gridsquares = JObject.Parse(json)["features"].Children();
+            Console.WriteLine($"{gridsquares.Count()} gridsquares in input file.");
+            var names = gridsquares.Select(s => s["properties"]["Name"]);
+            names.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}

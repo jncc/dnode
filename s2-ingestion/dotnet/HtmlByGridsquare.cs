59c113b818a6e793c02ecd6b5b68e1a96dfbfd63
Pete Montgomery <sumo.the.cat@gmail.com>
2018-01-03 15:14:55 +0000

    demo grid map

--- s2-ingestion/dotnet/HtmlByGridsquare.cs
+++ s2-ingestion/dotnet/HtmlByGridsquare.cs
@@ -45,24 +45,26 @@ namespace dotnet
             // ensure we have a feature gridsquare for every product-containing gridsquare
             Debug.Assert( gridsquaresWithProducts.All(s => gridsquaresInInputMap.Contains(s)) );
 
-            // filter feature gridsquares to only those with products, and remove extraneous properties
+            // filter feature gridsquares to only those with products, remove extraneous properties, add the product count
             var output = new {
                 type = "FeatureCollection",
                 name = "s2grid",
                 features = (from f in geojson["features"].Children()
                             let gridsquare = f["properties"]["Name"].ToString()
-                            where gridsquaresWithProducts.Contains(gridsquare)
+                            let productCount = (from grouping in productsByGridsquare
+                                                where grouping.Key == gridsquare
+                                                from product in grouping
+                                                select product).Count()
+                            where productCount > 0
                             select new {
                                 type = "Feature",
                                 geometry = f["geometry"],
-                                properties = new {
-                                    Name = gridsquare,
-                                    ProductCount = productsByGridsquare.Single(grouping => grouping.Key == gridsquare).Count()
-                                },
+                                properties = new { Name = gridsquare, ProductCount = productCount },
                             }).ToArray()
             };
 
-            File.WriteAllText(Path.Combine(outputDir, "grid.json"), JsonConvert.SerializeObject(output));
+            string js = $"var grid = {JsonConvert.SerializeObject(output)};";
+            File.WriteAllText(Path.Combine(outputDir, "grid.json.js"), js);
         }
 
         static void GenerateHtmlPages(List<IGrouping<string, Product>> productsByGridsquare)
@@ -73,22 +75,42 @@ namespace dotnet
                         <head>
                         <title>Sentinel-2 ARD index by gridsquare</title>
                         <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css""/>
+                        <link rel=""stylesheet"" href=""https://unpkg.com/leaflet@1.1.0/dist/leaflet.css""
+                            integrity=""sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==""
+                            crossorigin=""""/>
+                        <script src=""https://unpkg.com/leaflet@1.1.0/dist/leaflet.js""
+                            integrity=""sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==""
+                            crossorigin=""""></script>
+                        <script src=""grid.json.js"" type=""text/javascript""></script>
                         <style>
-                            td {
-                                border-bottom: 1px solid #bfc1c3;
-                                padding-bottom: 0.5em;
-                                text-align: left;
-                            }
-                            tr {
-                                border-bottom: 1px solid #bfc1c3;
-                                margin-bottom: 1em;
-                            }
-                        </style>        
+                            html, body { height: 100%; }
+                            #map { height: 90%; width:100%; }
+                        </style>
                         </head>
                         <body>
-                        <div class=""ui container"">
+                        <div class=""ui grid container"">
                         <br />
-                        <h1>Sentinel-2 ARD index by gridsquare</h1>");
+                        <h1>Sentinel-2 ARD index by gridsquare</h1>
+                        <div class=""ten wide column"">
+                            <div id=""map""></div>
+                        </div>
+                        <script>
+                            var url = 'https://api.mapbox.com/styles/v1/sumothecat/cj5w3i6w672u12slb33spg3te/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3Vtb3RoZWNhdCIsImEiOiJjaWxocngyanYwMDY4dmprcTg4ODN2Z3B2In0.CockfZdHAzqOfsbw8VcQyQ';
+                            var map = L.map('map').setView([54.5, -4], 5);
+                            L.tileLayer(url, { maxZoom: 20 }).addTo(map);
+                            function onEachFeature(feature, layer) {
+                                layer.on('click', function(e) {
+                                    window.open(feature.properties.Name + '.html');
+                                });
+
+                                layer.bindTooltip(feature.properties.Name, {
+                                    permanent: true
+                                })
+                            }
+                            L.geoJSON(grid, { onEachFeature: onEachFeature }).addTo(map);
+                        </script>
+                        <div class=""six wide column"">
+                        ");
 
 
             foreach (var productsInGridsquare in productsByGridsquare)
@@ -102,7 +124,7 @@ namespace dotnet
                 GenerateGridsquarePage(gridsquare, productsInGridsquare);
             }
 
-            s.Append("</div></body></html>");
+            s.Append("</div></div></body></html>");
 
             File.WriteAllText(Path.Combine(outputDir, "index.html"), s.ToString());
         }

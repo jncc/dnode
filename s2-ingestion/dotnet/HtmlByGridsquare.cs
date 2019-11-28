95403578681c293a48b6589d9c4b6e028c5131b7
Matt Debont <matt.debont@jncc.gov.uk>
2019-11-28 15:43:12 +0000

    Pushing an update to leaflet to try and resolve map error, mostly via the static height value

--- s2-ingestion/dotnet/HtmlByGridsquare.cs
+++ s2-ingestion/dotnet/HtmlByGridsquare.cs
@@ -72,66 +72,73 @@ namespace dotnet
             var s = new StringBuilder();
 
             s.Append(@"<html>
-                        <head>
-                        <!-- Google Tag Manager -->
-                        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-5TGXBJF');</script>
-                        <!-- End Google Tag Manager -->
-                        <title>Sentinel-2 ARD index by gridsquare</title>
-                        <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css""/>
-                        <link rel=""stylesheet"" href=""https://unpkg.com/leaflet@1.1.0/dist/leaflet.css""
-                            integrity=""sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==""
-                            crossorigin=""""/>
-                        <script src=""https://unpkg.com/leaflet@1.1.0/dist/leaflet.js""
-                            integrity=""sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==""
-                            crossorigin=""""></script>
-                        <script src=""grid.json.js"" type=""text/javascript""></script>
-                        <style>
-                            html, body { height: 100%; }
-                            #map { height: 90%; width:100%; }
-                        </style>
-                        </head>
-                        <body>
-                        <!-- Google Tag Manager (noscript) -->
-                        <noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5TGXBJF'
-                        height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
-                        <!-- End Google Tag Manager (noscript) -->
-                        <div class=""ui grid container"">
-                        <br />
-                        <h1>Sentinel-2 ARD index by gridsquare</h1>
-                        <br />
-                        <div class=""ten wide column"">
-                            <div id=""map""></div>
-                        </div>
-                        <script>
-                            var url = 'https://{s}.tiles.mapbox.com/v4/petmon.lp99j25j/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGV0bW9uIiwiYSI6ImdjaXJLTEEifQ.cLlYNK1-bfT0Vv4xUHhDBA';
-                            var map = L.map('map').setView([54.5, -4], 6);
-                            L.tileLayer(url, { maxZoom: 20 }).addTo(map);
-                            function onEachFeature(feature, layer) {
+    <head>
+        <!-- Google Tag Manager -->
+        <script>
+            (function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'}); 
+            var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; 
+            j.async = true; j.src ='https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-5TGXBJF');
+        </script>
+        <!-- End Google Tag Manager -->
+        <title>Sentinel-2 ARD index by gridsquare</title>
+        <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"" />
+        <link rel=""stylesheet"" href=""https://unpkg.com/leaflet@1.6.0/dist/leaflet.css""
+            integrity=""sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==""
+            crossorigin="""" />
+        <script src=""https://unpkg.com/leaflet@1.6.0/dist/leaflet.js""
+            integrity=""sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==""
+            crossorigin=""""></script>
+        <script src=""grid.json.js"" type=""text/javascript""></script>
+        <style>
+            html,body {
+                height: 100%;
+            }
+            #map {
+                height: 800px;
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5TGXBJF' height='0' width='0'
+                style='display:none;visibility:hidden'></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <div class=""ui grid container"">
+        <br />
+        <h1>Sentinel-2 ARD index by gridsquare</h1>
+        <br />
+        <div class=""ten wide column"">
+            <div id=""map""></div>
+        </div>
+        <script>
+            var url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            var map = L.map('map').setView([54.5, -4], 6);
+            L.tileLayer(url, { maxZoom: 20 }).addTo(map);
+            function onEachFeature(feature, layer) {
 
-                                var label = ""<a>"" + feature.properties.Name + ""</a>"";
+                var label = ""<a>"" + feature.properties.Name + ""</a>"";
 
-                                layer.bindTooltip(label, {
-                                    permanent: true,
-                                    interactive: true
-                                })
-                                layer.on('click', function(e) {
-                                    window.open(feature.properties.Name + '.html');
-                                });
-                                layer.on('mouseover', function() {
-                                    layer.setStyle( { color: '#ff3232' } );
-                                });
-                                layer.on('mouseout', function() {
-                                    layer.setStyle( { color: '#66a5ff' } );
-                                });
-                            }
-                            L.geoJSON(grid, { onEachFeature: onEachFeature }).addTo(map);
-                        </script>
-                        <div class=""six wide column"">
-                        ");
+                layer.bindTooltip(label, {
+                    permanent: true,
+                    interactive: true
+                })
+                layer.on('click', function (e) {
+                    window.open(feature.properties.Name + '.html');
+                });
+                layer.on('mouseover', function () {
+                    layer.setStyle({ color: '#ff3232' });
+                });
+                layer.on('mouseout', function () {
+                    layer.setStyle({ color: '#66a5ff' });
+                });
+            }
+            L.geoJSON(grid, { onEachFeature: onEachFeature }).addTo(map);
+        </script>
+        <div class=""six wide column"">
+        ");
 
 
             foreach (var productsInGridsquare in productsByGridsquare)
